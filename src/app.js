e6b0d4d4ad6ae44553ea558a560904b1dc0e3392
Vishal Goswami <vg0767875@gmail.com>
2026-08-12 17:09:17 +0530

    feat: add student routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import middleware  from "./middleware/error.middleware.js";
+import studentRoutes from "./routes/student.routes.js";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json())
+app.use("/api/student", studentRoutes);
 
 export default app;
