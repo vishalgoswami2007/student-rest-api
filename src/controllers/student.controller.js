19c42aec6aeff3c1d30abaf50e5a774fdfcb6ad8
Vishal Goswami <vg0767875@gmail.com>
2026-08-14 17:54:47 +0530

    feat: add get student by id

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -35,4 +35,29 @@ const getStudents = async(req,res) => {
     }
 };
 
-export { createStudent , getStudents };
+const getStudentById = async (req, res) => {
+    try {
+        const student = await Student.findById(req.params.id);
+
+        if (!student) {
+            return res.status(404).json({
+                success: false,
+                message: "Student not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Student found successfully",
+            data: student
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+export { createStudent , getStudents , getStudentById };
--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getStudents , createStudent } from "../controllers/student.controller.js";
+import { getStudents , createStudent  , getStudentById} from "../controllers/student.controller.js";
 
 const router = express.Router();
 
 router.post("/", createStudent)
 router.get("/" , getStudents)
+router.get("/:id" , getStudentById)
 
 export default router;
